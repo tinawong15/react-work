be0f8dbd2391daafad537dcdac679a247b0faa72
tinawong15 <tinaw425@gmail.com>
2020-06-14 17:38:39 -0400

    completed 2.14

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import axios from 'axios'
 const App = () => {
     const [showAll, setShowAll] = useState('')
     const [result, setResult] = useState([])
+    const [preview, setPreview] = useState(false)
 
     const handleFilterChange = (event) => {
         setShowAll(event.target.value)
+        setPreview(false)
     }   
 
     useEffect(() => { 
@@ -24,10 +26,29 @@ const App = () => {
         
     }, [showAll])
 
-    const displayLanguages = () => {
-        const display = result[0].languages.map(language =>
+    const displayLanguages = (country) => {
+        const display = country.languages.map(language =>
             <li key={language.name}>{language.name}</li>)
         return display
+    }
+    const handleClick = (country) => {
+        setPreview(country)
+    }
+
+    const showPreview = () => {
+        if(preview !== false) {
+            return (<div>
+                    <h1>{preview.name}</h1>
+                    <p>capital: {preview.capital}</p>
+                    <p>population: {preview.population}</p>
+                    <h3>languages</h3>
+                    <ul>{displayLanguages(preview)}</ul>
+                    <img src={preview.flag} width="100px" alt=""/>
+                </div>)
+        }
+        else {
+            return <div></div>
+        }
     }
     const displayCountries = () => {
         if(result.length >= 10) {
@@ -35,7 +56,7 @@ const App = () => {
         }
         else if(result.length > 1) {
             const display = result.map(country =>
-                <div key={country.numericCode}>{country.name}</div>)
+                <div key={country.numericCode}>{country.name}<button onClick={()=>handleClick(country)}>show</button></div>)
             return display
         }
         else if(result.length === 1) {
@@ -45,7 +66,7 @@ const App = () => {
                 <p>capital: {result[0].capital}</p>
                 <p>population: {result[0].population}</p>
                 <h3>languages</h3>
-                <ul>{displayLanguages()}</ul>
+                <ul>{displayLanguages(result[0])}</ul>
                 <img src={result[0].flag} width="100px" alt=""/>
             </div>
         }
@@ -58,6 +79,7 @@ const App = () => {
         <div>
             find countries <input value={showAll} onChange={handleFilterChange}/><br></br>
             {displayCountries()}
+            {showPreview()}
         </div>
     )
 }
